cada62cfe80b8b61d6c7c36f63bc09829e90eff3
Zourethe <pedro.zourethe@gmail.com>
2024-07-29 20:47:54 -0300

    Update renamer.py
    New renaming system created.

--- renamer.py
+++ renamer.py
@@ -13,18 +13,31 @@ from shutil import move
 
 # Variables definition.
 option = int()
+rawPath = str()
 
 # Lowercase function.
+def pathFixer(rawPath):
+    lastChar = rawPath[len(rawPath) - 1]
+    if lastChar == '/':
+        print(rawPath)
+        return(rawPath)
+    else:
+        path = rawPath + '/'
+        print(path)
+        return(path)
+
 def lowercase(path):
-    for filenames in listdir(path):
-        rename('{}\{}'.format(path, filenames), filenames.lower())
-        move(filenames.lower(), path)
+    for filename in listdir(path):
+        rename(path + filename, path + filename.lower())
 
 # Uppercase function.
 def uppercase(path):
-    for filenames in listdir(path):
-        rename('{}\{}'.format(path, filenames), filenames.upper())
-        move(filenames.upper(), path)
+    for filename in listdir(path):
+        rename(path + filename, path + filename.uppercase())
+
+def capitalize(path):
+    for filename in listdir(path):
+        rename(path + filename, path + filename.capitalize())
 
 # Options selector.
 print('\033[32m{}\033[0m'.format('                  Renamer'))
@@ -32,7 +45,8 @@ print('')
 print('Options:')
 print('1 - Uppercase.')
 print('2 - Lowercase.')
-print('3 - Exit.')
+print('3 - Capitalize')
+print('4 - Exit.')
 print('')
 print('Script by Zourethe')
 print('')
@@ -45,13 +59,17 @@ while True:
         print('Type only numbers.')
     if option == 1:
         path = str(input('Type the path: '))
-        uppercase(path)
+        uppercase(pathFixer(rawPath))
         break
     elif option == 2:
-        path = str(input('Type the path: '))
-        lowercase(path)
+        rawPath = str(input('Type the path: '))
+        lowercase(pathFixer(rawPath))
         break
     elif option == 3:
+        rawPath = str(input('Type the path: '))
+        capitalize(pathFixer(rawPath))
+        break
+    elif option == 4:
         quit()
     else:
         print('Incorret option, type another.')
